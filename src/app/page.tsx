02d805a7811442e581269be156af8fa91198a0dc
gideonadeti <gideonadeti0@gmail.com>
2025-03-14 10:19:39 +0000

    Add object-cover to avatar image

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -31,7 +31,11 @@ export default function Page() {
             </div>
             <BlurFade delay={BLUR_FADE_DELAY}>
               <Avatar className="size-28 border">
-                <AvatarImage alt={DATA.name} src={DATA.avatarUrl} />
+                <AvatarImage
+                  className="object-cover"
+                  alt={DATA.name}
+                  src={DATA.avatarUrl}
+                />
                 <AvatarFallback>{DATA.initials}</AvatarFallback>
               </Avatar>
             </BlurFade>
