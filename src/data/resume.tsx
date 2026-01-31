6adc2e37a84567aafacc00c48951e93f64a3996a
Gideon Adeti <gideonadeti0@gmail.com>
2026-01-31 22:00:56 +0000

    chore(resume): Remove website link from resume data

--- src/data/resume.tsx
+++ src/data/resume.tsx
@@ -140,11 +140,6 @@ export const DATA = {
       ],
       links: [
         {
-          type: "Website",
-          href: "https://reen-commerce.vercel.app",
-          icon: <Icons.globe className="size-3" />,
-        },
-        {
           type: "Source",
           href: "https://github.com/gideonadeti/reen-backend",
           icon: <Icons.github className="size-3" />,
