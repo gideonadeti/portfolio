60c27edd8a17f04543ffaef63c1b3ce1d198ce69
Gideon Adeti <gideonadeti0@gmail.com>
2026-02-27 22:01:34 +0000

    Update avatarUrl in resume data

--- src/data/resume.tsx
+++ src/data/resume.tsx
@@ -10,7 +10,7 @@ export const DATA = {
   description: "Full-Stack Software Engineer",
   summary:
     "I'm a Full-Stack Software Engineer with experience in designing, developing, deploying, and maintaining web, mobile, desktop, and machine learning applications.",
-  avatarUrl: "",
+  avatarUrl: "https://res.cloudinary.com/ds1z7r1u8/image/upload/v1772229538/IMG-20260221-WA02011_ra4cyf.jpg",
   skills: [
     "Web Applications",
     "Mobile Applications",
