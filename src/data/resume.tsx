0d95fde6d6dee9d9db402c92608be9a15dd3be69
Gideon Adeti <gideonadeti0@gmail.com>
2026-03-08 20:43:15 +0000

    feat(resume): Add Projects link to navbar with corresponding icon

--- src/data/resume.tsx
+++ src/data/resume.tsx
@@ -1,5 +1,5 @@
 import { Icons } from "@/components/icons";
-import { HomeIcon, NotebookIcon } from "lucide-react";
+import { FolderKanbanIcon, HomeIcon, NotebookIcon } from "lucide-react";
 
 export const DATA = {
   name: "Gideon Adeti",
@@ -20,6 +20,7 @@ export const DATA = {
   ],
   navbar: [
     { href: "/", icon: HomeIcon, label: "Home" },
+    { href: "/projects", icon: FolderKanbanIcon, label: "Projects" },
     { href: "/blog", icon: NotebookIcon, label: "Blog" },
   ],
   contact: {
