5254e9b8eb7004b7674dda23beef57deda6654af
Gideon Adeti <gideonadeti0@gmail.com>
2026-03-19 12:24:52 +0000

    Update resume description to include neuroengineering

--- src/data/resume.tsx
+++ src/data/resume.tsx
@@ -7,7 +7,7 @@ export const DATA = {
   url: "https://gideonadeti.vercel.app",
   location: "Accra, Greater Accra Region, Ghana",
   locationLink: "https://www.google.com/maps/place/Accra",
-  description: "Full-Stack Software Engineer",
+  description: "Full-Stack Software Engineer & Aspiring Neuroengineer",
   summary:
     "I'm a Full-Stack Software Engineer with experience in designing, developing, deploying, and maintaining web, mobile, desktop, and machine learning applications. I'm also an aspiring neuroengineer.",
   avatarUrl:
